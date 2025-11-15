50b451d8ef346ce9ab3d6530e40d014fe1c53223
Nags <nagpichikaganesh@gmail.com>
2025-11-16 04:40:28 +0530

    LineChart - aligned

--- Frontend/src/components/dashboard/EngineVibrationSimulator.tsx
+++ Frontend/src/components/dashboard/EngineVibrationSimulator.tsx
@@ -32,20 +32,27 @@ const series = [
 ];
 export default function GridDemo() {
   return (
+<div className="flex-col relative bg-racing-panel/50 rounded-2xl border border-racing-border/50 backdrop-blur-sm flex justify-center items-center pr-10 py-2">
+  <div className="w-full max-w-[800px]"> 
     <LineChart
       dataset={usUnemploymentRate}
       xAxis={xAxis}
       yAxis={yAxis}
       series={series}
       height={300}
+      width={undefined}
       grid={{ vertical: true, horizontal: true }}
       sx={{
         [`& .${chartsGridClasses.line}`]: {
-          stroke: '#999',  
-          strokeDasharray: '5 3',
+          stroke: "#999",
+          strokeDasharray: "5 3",
           strokeWidth: 2,
         },
       }}
-      />
+    />
+  </div>
+  <span className='font-mono font-bold'>Engine Vibration</span>
+</div>
+
   );
 }
